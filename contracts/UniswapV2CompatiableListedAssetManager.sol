3751fd19bc926d23178f2c9132b84c253a80e8f0
cyotee doge <cyotee@gmail.com>
2020-12-13 17:58:21 -0800

    Syncing submodules

--- contracts/UniswapV2CompatiableListedAssetManager.sol
+++ contracts/UniswapV2CompatiableListedAssetManager.sol
@@ -10,7 +10,7 @@ import "./dependencies/holyzeppelin/contracts/math/Counters.sol";
 contract UniswapV2CompatiableListedAssetManager is Ownable {
     
     using SafeMath for uint256;
-    using Counts for Counters.Counter;
+    using Counters for Counters.Counter;
 
     mapping (address => mapping (address => address)) private _managedUniswapV2CompatiablePairs;
 
@@ -19,5 +19,17 @@ contract UniswapV2CompatiableListedAssetManager is Ownable {
     function addManagedAsset( address token1_, address token2_, address uniswapV2CompatiableLPToken_ ) external onlyOwner() {
         _managedUniswapV2CompatiablePairs[token1_][token2_] = uniswapV2CompatiableLPToken_;
         _managedUniswapV2CompatiablePairs[token2_][token1_] = uniswapV2CompatiableLPToken_;
+    }
+
+    function raiseTokenPrice( address tokenToRaise_, uint256 amountToRaiseBy_ ) external onlyOwner() {
+
+    }
+
+    function lowerTokenPrice( address tokenToLower_, uint256 amountToLowerBy_ ) external onlyOwner() {
+
+    }
+
+    function exchangeLiquidty( address tokenToRemove_, address opposingTokenToRemove_, address tokenToDeposit_, address opposingTokenToDeposit_ ) external onlyOwner() {
+        
     }
 }
