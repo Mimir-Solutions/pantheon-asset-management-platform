aed97a12a076507e9c34086dadae8d9fc933df01
cyotee doge <cyotee@gmail.com>
2020-12-16 04:26:27 -0800

    Added asset vault scaffold

---
+++ contracts/vaults/interfaces/IPantheonAssetVault.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+import "hardhat/console.sol";
+
+contract IPantheonSalesPlatformAssetVault is IERC721Receiver, ERC165Enhanced {
+
+    uint256 public totalValueManaged
+
+    constructor() {}
+
+    // ERC20 Asset Management functions
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    // ERC721 Asset Management functions
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+
+    function transferFrom(address from, address to, uint256 tokenId) external;
+
+    function approve(address to, uint256 tokenId) external;
+
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    function setApprovalForAll(address operator, bool _approved) external;
+
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+}
